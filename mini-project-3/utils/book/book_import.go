f75bf3e5d6a1b19fb66a5878e5ce642d8f41ed7d
PGPY <putu.yasa2@gmail.com>
2024-03-26 00:23:07 +0800

    update mini project 3

--- mini-project-3/utils/book/book_import.go
+++ mini-project-3/utils/book/book_import.go
@@ -15,69 +15,39 @@ func BookImport() {
 	pathFile := ""
 	helpers.LineInput("Masukan path file yang ingin diimport : ", &pathFile)
 
-	file, err := os.Open(pathFile)
+	file, err := os.Open("./imports/sample_books.csv")
 	if err != nil {
 		fmt.Println("Terjadi Error:", err)
 		BookImport()
 	}
 	defer file.Close()
 
-	reader := csv.NewReader(file)
-	lines, err := reader.ReadAll()
-	if err != nil {
-		fmt.Println("Terjadi Error:", err)
-		return
-	}
-
 	fmt.Println("Menunggu import data buku ...")
 
-	wg := sync.WaitGroup{}
-
-	ch := make(chan []string)
-	chBooks := make(chan model.Book, len(lines))
-
-	jmlThread := 5
-
-	for i := 0; i < jmlThread; i++ {
-		wg.Add(1)
-		go func(ch <-chan []string, chBooks chan model.Book, wg *sync.WaitGroup) {
-			for book := range ch {
-				chBooks <- model.Book{
-					Model: model.Model{
-						ID: uint(convertToInt(book[0])),
-					},
-					ISBN:    book[1],
-					Penulis: book[2],
-					Tahun:   uint(convertToInt(book[3])),
-					Judul:   book[4],
-					Gambar:  book[5],
-					Stok:    uint(convertToInt(book[6])),
-				}
-			}
-			wg.Done()
-		}(ch, chBooks, &wg)
+	csvChan, err := loadFile(file)
+	if err != nil {
+		fmt.Println("Terjadi Error:", err)
 	}
 
-	for i, book := range lines {
-		if i == 0 {
-			continue
-		}
-		ch <- book
+	jmlGoroutine := 10
+
+	var bookChanTemp []<-chan model.Book
+
+	for i := 0; i < jmlGoroutine; i++ {
+		bookChanTemp = append(bookChanTemp, convertStruct(csvChan))
 	}
 
-	close(ch)
-	wg.Wait()
-	close(chBooks)
+	mergedCh := appendBooks(bookChanTemp...)
 
-	for book := range chBooks {
-		_, err := book.GetByID(config.Mysql.DB)
+	for chBook := range mergedCh {
+		_, err := chBook.GetByID(config.Mysql.DB)
 		if err != nil {
-			err := book.Create(config.Mysql.DB)
+			err := chBook.Create(config.Mysql.DB)
 			if err != nil {
 				fmt.Println(err)
 			}
 		} else {
-			err := book.UpdateByID(config.Mysql.DB)
+			err := chBook.UpdateByID(config.Mysql.DB)
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -92,4 +62,74 @@ func convertToInt(value string) int {
 		fmt.Println(err)
 	}
 	return result
+}
+
+func loadFile(file *os.File) (<-chan []string, error) {
+	bookChan := make(chan []string)
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		return bookChan, err
+	}
+
+	go func() {
+		for i, book := range records {
+			if i == 0 {
+				continue
+			}
+			bookChan <- book
+		}
+		close(bookChan)
+	}()
+
+	return bookChan, nil
+
+}
+
+func convertStruct(csvChan <-chan []string) <-chan model.Book {
+	booksChan := make(chan model.Book)
+
+	go func() {
+		for book := range csvChan {
+			booksChan <- model.Book{
+				Model: model.Model{
+					ID: uint(convertToInt(book[0])),
+				},
+				ISBN:    book[1],
+				Penulis: book[2],
+				Tahun:   uint(convertToInt(book[3])),
+				Judul:   book[4],
+				Gambar:  book[5],
+				Stok:    uint(convertToInt(book[6])),
+			}
+		}
+
+		close(booksChan)
+	}()
+
+	return booksChan
+}
+
+func appendBooks(bookChanMany ...<-chan model.Book) <-chan model.Book {
+	wg := sync.WaitGroup{}
+
+	mergedChan := make(chan model.Book)
+
+	wg.Add(len(bookChanMany))
+	for _, ch := range bookChanMany {
+		go func(ch <-chan model.Book) {
+			for books := range ch {
+				mergedChan <- books
+			}
+			wg.Done()
+		}(ch)
+	}
+
+	go func() {
+		wg.Wait()
+		close(mergedChan)
+	}()
+
+	return mergedChan
 }
